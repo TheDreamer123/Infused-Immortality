e8f53b57fd4ab731688123fb97811a0e6377f4c6
TheDreamer123 <93482793+TheDreamer123@users.noreply.github.com>
2022-08-23 14:21:51 +0100

    Update InfusedimmortalityRecipeSerializer.java

--- src/main/java/net/dreamer/infusedimmortality/recipe/InfusedimmortalityRecipeSerializer.java
+++ src/main/java/net/dreamer/infusedimmortality/recipe/InfusedimmortalityRecipeSerializer.java
@@ -5,11 +5,11 @@ import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public interface InfusedimmortalityRecipeSerializer {
-    SpecialRecipeSerializer<InfusedTotemRecipe> INFUSED_TOTEM = new SpecialRecipeSerializer<>(InfusedTotemRecipe::new);
-    SpecialRecipeSerializer<TotemOfUndyingRecipe> TOTEM = new SpecialRecipeSerializer<>(TotemOfUndyingRecipe::new);
+public class InfusedimmortalityRecipeSerializer {
+    public static SpecialRecipeSerializer<InfusedTotemRecipe> INFUSED_TOTEM = new SpecialRecipeSerializer<>(InfusedTotemRecipe::new);
+    public static SpecialRecipeSerializer<TotemOfUndyingRecipe> TOTEM = new SpecialRecipeSerializer<>(TotemOfUndyingRecipe::new);
 
-    static void register() {
+    public static void register() {
         Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(InfusedImmortality.MOD_ID, "crafting_special_infused_totem"),INFUSED_TOTEM);
         Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(InfusedImmortality.MOD_ID, "crafting_special_totem_of_undying"),TOTEM);
     }
